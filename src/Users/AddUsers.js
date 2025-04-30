41bcbf2ac9a534d7d105b9b09a1dc833aa349468
senith <senithdakshina@gmail.com>
2025-04-30 14:50:16 +0530

    complete

--- src/Users/AddUsers.js
+++ src/Users/AddUsers.js
@@ -1,6 +1,11 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function AddUsers() {
+
+  let navigate=useNavigate()
+
   const[user,setUser] = useState({
     name:"",
     username:"",
@@ -13,11 +18,18 @@ const onInputChange=(e)=>{
   setUser({...user,[e.target.name]:e.target.value})
 }
 
+const onSubmit=async(e)=>{
+e.preventDefault();
+await axios.post("http://localhost:8080/user",user)
+navigate("/");
+}
+
   return (
     <div className='container'>
         <div className='row'>
             <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
                 <h2 className='text-center m-4'>Register User</h2>
+                <form onSubmit={(e)=>onSubmit(e)}>
 
                 <div className='mb3'> 
                   <label htmlFor='Name' className='form-label'> Name</label>
@@ -42,6 +54,7 @@ const onInputChange=(e)=>{
 
                 <button type='submit' className='btn btn-outline-primary'>Submit</button>
                 <button type='submit' className='btn btn-outline-danger mx-2'>Cancel</button>
+                </form>
 
             </div>
         </div>
